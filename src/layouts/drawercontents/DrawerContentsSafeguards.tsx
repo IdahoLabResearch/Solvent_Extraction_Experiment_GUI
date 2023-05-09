07fd14f6f630cb341075df7a95f1e3a811b685ee
Adam Pluth <adam.pluth@inl.gov>
2023-05-09 15:19:08 -0600

    added safeguard status to sidebar

--- src/layouts/drawercontents/DrawerContentsSafeguards.tsx
+++ src/layouts/drawercontents/DrawerContentsSafeguards.tsx
@@ -4,26 +4,74 @@ import * as React from 'react';
 // Hooks
 import { useAppSelector } from '../../app/hooks/reduxTypeScriptHooks';
 
+// API calls
+import { useFetchContactorsQuery } from '../../app/services/contactorsDataApi';
+
+// Import Packages
+import { DateTime } from 'luxon';
+
 // MUI Components
 import {
   Box,
+  Typography
 } from '@mui/material';
+
+// Custom Components
+import StatusBox from '../../components/status/StatusBox';
+
+// Import styles
+import '../../styles/App.scss';
+// @ts-ignore
+import COLORS from '../../styles/variables';
 
 type Props = {};
 
 const DrawerContentsSafeguards: React.FC<Props> = ({}) => {
-  type openDrawerLeftState = boolean;
-  const openDrawerLeftState: openDrawerLeftState = useAppSelector((state: any) => state.appState.openDrawerLeft);
+  type contactorList = Array<{ [key: string]: any; }>;
+  const { data: contactorList } = useFetchContactorsQuery();
 
-  type openDrawerLeftWidth = number;
-  const openDrawerLeftWidth: openDrawerLeftWidth = useAppSelector((state: any) => state.appState.openDrawerLeftWidth);
+  type contactorListWithErrors = Array<{ [key: string]: any; }>;
+  const contactorListWithErrors = new Array(contactorList?.filter((contactor: any) => contactor.mlStatus !== null));
+  const alarmBoolean = () => contactorListWithErrors?.length > 0;
 
   return (
-    <Box
-      sx={{ display: 'flex', flexDirection: 'column', overflowX: 'hidden', }}
-    >
-      <Box sx={{ flex: 1, minHeight: 0, overflowX: 'hidden', overflowY: 'auto', padding: '0 16px 16px' }}>
-        Safeguards go here
+    <Box sx={{ padding: '16px'}}>
+      <Box
+        sx={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          alignItems: 'stretch',
+          flexDirection: 'row',
+          alignContent: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <StatusBox alarm={alarmBoolean()} />
+        <Box
+          sx={{
+            display: 'flex', flex: '1 100%', p: 3, backgroundColor: '#121212', alignItems: 'center',
+          }}
+        >
+          <Typography 
+            sx={{  
+              ...(alarmBoolean() === false && {
+                color: COLORS.colorSuccess
+              }),
+              ...(alarmBoolean() === true && {
+                color: COLORS.colorError
+              }),
+            }}
+          >
+            {alarmBoolean() === false
+              ? (
+                <span>Optimal system function</span>
+              ) : (
+                <span><strong>Alarm:&nbsp;</strong>Investigation needed</span>
+              )
+            }
+          </Typography>
+        </Box>
       </Box>
     </Box>
   );
